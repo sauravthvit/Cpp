7ef2c0846b8af6cf5598648a554e7530fcc3eff3
My Name <sauravth2003@gmail.com>
2024-03-10 15:19:00 +0530

    Updated

--- 03variables.cpp
+++ 03variables.cpp
@@ -7,13 +7,13 @@ int main() {
 
     //Initialised variables
 
-    int room_width {0};
-    int room_length {0};
 
     cout << "Enter the width of the room: ";
+    int room_width {0};
     cin >> room_width;
 
     cout << "Enter the length of the room: ";
+    int room_length {0};
     cin >> room_length;
 
     cout << "The area of the room is " << room_width * room_length << " sqft." << endl;
