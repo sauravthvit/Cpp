780bcd08fa0f9edfdf624bd218b48b57b01bd9a6
My Name <sauravth2003@gmail.com>
2024-03-10 15:29:42 +0530

    Global variables

---
+++ 04globalvariables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//This program depicts the use of global variables
+
+int main() {
+
+    int age {18}; //Global variable
+
+    cout << age << endl;
+
+    return 0;
+}
